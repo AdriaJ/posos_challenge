38fc15c4e3c39cafc346e540d1e01f1ce3508a30
AdriaJ <adrian.jarret@student.ecp.fr>
2019-12-02 18:08:31 +0100

    Finished project

--- Makefile
+++ Makefile
@@ -7,15 +7,17 @@ decrypt:
 
 .PHONY: build_train
 build_train: decrypt
-	docker build -f train2.Dockerfile\
+	docker build -f trainer/trainer.Dockerfile\
 	 							--tag $(NAME)_train\
 								.
 
 .PHONY: train
 train: build_train clean_train
-	docker run -it --rm -v $(PWD):/app\
-								--name=$(NAME)_train\
-								 $(NAME)_train:latest
+	docker run -it --rm -v $(PWD)/trainer:/app\
+						-v $(PWD)/data:/data\
+						-v $(PWD)/models:/models\
+						--name=$(NAME)_train\
+						 $(NAME)_train:latest
 
 
 .PHONY: clean_train
@@ -23,17 +25,19 @@ clean_train:
 	docker rm -f $(NAME)_train 2> /dev/null || true
 
 
-.PHONY: buil_api
-buil_api:
-	docker build -f api.Dockerfile\
-	 							--tag $(NAME)_api\
-								.
+.PHONY: build_api
+build_api:
+	docker build -f api/api.Dockerfile\
+	 			--tag $(NAME)_api\
+				.
 
 .PHONY: api
 api: build_api clean_api
-	docker run -it --name $(NAME)_api\
-							-p 4002:4002\
-							$(NAME)_api:latest
+	docker run -d --name $(NAME)_api\
+	            -v $(PWD)/api:/app\
+	            -v $(PWD)/models:/models\
+				-p 4002:4002\
+				$(NAME)_api:latest
 
 
 
---
+++ trainer/train_model.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix
+
+from joblib import dump
+
+data_path = "/data"
+in_train_path = os.path.join(data_path, 'input_train.csv')
+out_train_path = os.path.join(data_path, 'output_train.csv')
+# read data
+X = pd.read_csv(in_train_path, index_col=0)
+y = pd.read_csv(out_train_path, index_col=0)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=.8)
+
+print("Training model ...")
+
+# vectorize
+vectorizer = TfidfVectorizer()
+
+X_train_v = vectorizer.fit_transform(X_train["question"])
+
+# classifier
+clf = svm.SVC(gamma='scale', probability=True)
+clf.fit(X_train_v, y_train['intention'].to_numpy())
+
+print("Done training!")
+
+# Evaluating the model :
+X_val_v = vectorizer.transform(X_val['question'])
+y_val_pred = clf.predict(X_val_v)
+
+# print("Precision - Recall - F1-Score (over validation set):")
+# print("Macro:")
+# print(precision_recall_fscore_support(y_val.to_numpy().reshape(-1,), y_val_pred, average='macro')[:3])
+# print("Micro:")
+# print(precision_recall_fscore_support(y_val.to_numpy().reshape(-1,), y_val_pred, average='micro')[:3])
+#
+# print("Confusion matrix (over validation set):")
+# cm = confusion_matrix(y_val.to_numpy().reshape(-1,), y_val_pred)
+# print(cm)
+
+
+# exporting the model
+
+model_path = "/models"
+export_path = os.path.join(model_path, "model_svm")
+print('Exporting trained model to', export_path)
+dump(clf, export_path)
+export_path = os.path.join(model_path, "vectorizer")
+dump(vectorizer, export_path)
+print("Done exporting!")
